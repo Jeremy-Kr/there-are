02529cf05d560f63129352bacc292d8318d7ede7
Jeremy-kr <wjddlr905@gmail.com>
2023-01-06 17:32:24 +0900

    fix(Stacks.jsx): fix stack components dependencies

--- navigation/Stacks.jsx
+++ navigation/Stacks.jsx
@@ -1,11 +1,19 @@
-import styled from '@emotion/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Splash from '../screen/Splash';
+import Login from '../screen/Login';
+import SignUp from '../screen/SignUp';
+import Landing from '../screen/Landing';
+import AddToBe from '../screen/AddToBe';
+import Detail from '../screen/Detail';
+import AddDetail from '../screen/AddDetail';
+import EditNickName from '../screen/EditNickName';
 
 const Stack = createNativeStackNavigator();
 
 const Stacks = ({ navigation: { goBack } }) => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Splash" component={Splash} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="Landing" component={Landing} />
