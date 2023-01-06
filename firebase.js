3d539f2aa32810f717dffa6a14173b8768ad1ebe
Jeremy-kr <wjddlr905@gmail.com>
2023-01-06 14:50:45 +0900

    init

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkDCqGlkeukNDH14uRt5bPnC3nUxL0bZk",
+  authDomain: "there-are-323ae.firebaseapp.com",
+  projectId: "there-are-323ae",
+  storageBucket: "there-are-323ae.appspot.com",
+  messagingSenderId: "337725371789",
+  appId: "1:337725371789:web:8460c7eaa0e6b6e343f9c4",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
