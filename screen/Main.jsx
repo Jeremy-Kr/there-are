1c78cd6aa2e02f0a9dc80f8cfe366038d5fd59f4
HongguLim <Honggu.Lim@outlook.com>
2023-01-09 15:58:13 +0900

    refactor: ui/main css refactoring

--- screen/Main.jsx
+++ screen/Main.jsx
@@ -8,49 +8,35 @@ import { CustomH1 } from '../components/Common/CustomText';
 
 const Main = ({ navigation: { navigate } }) => {
   return (
-    <SafeAreaView>
-      <MainContainer>
-        <ListedContainer>
-          <ListedText1>나 이순신은 전쟁천재이다.</ListedText1>
-          <ListedText2>나 이순신은 기록 장인이다.</ListedText2>
-          <ListedText3>나 이순신은 애국자다.</ListedText3>
-        </ListedContainer>
-      </MainContainer>
+    <MainContainer>
+      <ListedContainer>
+        <ListedText>나 이순신은 전쟁천재이다.</ListedText>
+        <ListedText>나 이순신은 기록 장인이다.</ListedText>
+        <ListedText>나 이순신은 애국자다.</ListedText>
+      </ListedContainer>
       <ButtonContainer>
         <CustomButton>추가하기</CustomButton>
       </ButtonContainer>
-    </SafeAreaView>
+    </MainContainer>
   );
 };
 
-const MainContainer = styled.View`
-  height: 700px;
+const MainContainer = styled.SafeAreaView`
   justify-content: center;
 `;
 
 const ListedContainer = styled.View`
   align-items: center;
+  margin-top: 250px;
 `;
 
 const ButtonContainer = styled.View`
   align-items: center;
   justify-content: center;
-  position: absolute;
-  width: 74px;
-  height: 27px;
-  left: 160px;
-  top: 676px;
+  margin-top: 150px;
 `;
 
-const ListedText1 = styled(CustomH1)`
-  line-height: 44px;
-  margin: 7px;
-`;
-const ListedText2 = styled(CustomH1)`
-  line-height: 44px;
-  margin: 7px;
-`;
-const ListedText3 = styled(CustomH1)`
+const ListedText = styled(CustomH1)`
   line-height: 44px;
   margin: 7px;
 `;
