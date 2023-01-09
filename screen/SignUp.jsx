39b4b7ee6fd66661f8025f6698a3a60b4bbfe42a
hongdakyung <bongbk72@naver.com>
2023-01-09 12:11:45 +0900

    Feat : UI SignUp page
    SignUp page의 ui 작업을 진행하였습니다
    
    by ddoqi

--- screen/SignUp.jsx
+++ screen/SignUp.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import styled from '@emotion/native';
+import ThereAreMainText from '../components/ThereAre/ThereAreMainText';
+import CustomInput from '../components/Common/CustomInput';
+import { CustomH4 } from '../components/Common/CustomText';
+import CustomButton from '../components/Common/CustomButton';
 
 const SignUp = () => {
-  return <SignUpContainer></SignUpContainer>;
+  return (
+    <SignUpContainer>
+      <ThereAreMainText />
+      <InputContainer>
+        <InputText>이메일</InputText>
+        <CustomInput />
+        <InputText>비밀번호</InputText>
+        <CustomInput />
+        <InputText>이름</InputText>
+        <CustomInput />
+        <LoginBottomContainer>
+          <JoinButtonContainer>
+            <CustomButton>회원가입</CustomButton>
+          </JoinButtonContainer>
+          <InputText>이미 회원이신가요?</InputText>
+        </LoginBottomContainer>
+      </InputContainer>
+    </SignUpContainer>
+  );
 };
 
 export default SignUp;
@@ -12,3 +34,19 @@ const SignUpContainer = styled.SafeAreaView`
   justify-content: center;
   align-items: center;
 `;
+
+const InputContainer = styled.View`
+  margin: 10px;
+`;
+
+const InputText = styled(CustomH4)`
+  margin: 10px;
+`;
+
+const LoginBottomContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin: 30px;
+`;
+
+const JoinButtonContainer = styled.TouchableOpacity``;
