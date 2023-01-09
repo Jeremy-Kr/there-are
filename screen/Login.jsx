6d35f3e07b3792a898dfefd4a39dbd3d6b118e4d
hongdakyung <bongbk72@naver.com>
2023-01-09 17:02:25 +0900

    feat : Auth 서비스 로직 연결
    by ddoqi

--- screen/Login.jsx
+++ screen/Login.jsx
@@ -1,22 +1,95 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/native';
 import ThereAreMainText from '../components/Common/ThereAreMainText';
 import CustomInput from '../components/Common/CustomInput';
 import { CustomH4 } from '../components/Common/CustomText';
 import CustomButton from '../components/Common/CustomButton';
 import { TouchableOpacity } from 'react-native';
+//---------------------------------------------
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { authService } from '../firebase';
+import { emailRegex, pwRegex } from '../utils/utils';
+//---------------------------------------------
 
 const Login = ({ navigation: { navigate } }) => {
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  //유효성 검사 함수
+  const validateInputs = () => {
+    if (!email) {
+      console.log('이멜 입력하라능');
+      return true;
+    }
+    if (!pw) {
+      console.log('패스워드 입력하라능');
+      return true;
+    }
+    const matchedEmail = email.match(emailRegex);
+    const matchedPw = pw.match(pwRegex);
+
+    if (matchedEmail === null) {
+      console.log('이멜 형식 맞게 입력하라능');
+      return true;
+    }
+    if (matchedPw === null) {
+      console.log('패스워드 형식 맞게 입력하라능');
+      return true;
+    }
+  };
+
+  //로그인 기능 함수
+  const handleLogin = () => {
+    console.log('이거 왜 실행안되냐');
+    // 유효성 검사함수 실행시키기
+    if (validateInputs()) {
+      return;
+    }
+
+    // 로그인 요청
+    signInWithEmailAndPassword(authService, email, pw)
+      .then(() => {
+        console.log('로그인성공');
+        setEmail('');
+        setPw('');
+      })
+      .catch((err) => {
+        console.log('err.message:', err.message);
+        if (err.message.includes('user-not-found')) {
+          console.log('회원이 아닙니다 회원가입 진행해주세여');
+        }
+        if (err.message.includes('wrong-password')) {
+          console.log('비밀번호 틀렸슴둥');
+        }
+      });
+  };
+
   return (
     <LoginContainer>
       <ThereAreMainText />
       <InputContainer>
         <InputText>이메일</InputText>
-        <CustomInput />
+        <CustomInput
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          textContentType="emailAddress"
+          placeholder="Enter your email id"
+        />
         <InputText>비밀번호</InputText>
-        <CustomInput />
+        <CustomInput
+          value={pw}
+          onChangeText={(text) => setPw(text)}
+          textContentType="password"
+          returnKeyType="send"
+          secureTextEntry={true}
+          placeholder="Enter your password"
+        />
         <LoginBottomContainer>
-          <LoginButton>
+          <LoginButton
+            onPress={() => {
+              handleLogin();
+            }}
+          >
             <CustomButton>로그인</CustomButton>
           </LoginButton>
           <TouchableOpacity
--- screen/SignUp.jsx
+++ screen/SignUp.jsx
@@ -1,24 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/native';
 import ThereAreMainText from '../components/Common/ThereAreMainText';
 import CustomInput from '../components/Common/CustomInput';
 import { CustomH4 } from '../components/Common/CustomText';
 import CustomButton from '../components/Common/CustomButton';
 import { TouchableOpacity } from 'react-native';
+//-------------------------------------------------------------------
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { authService } from '../firebase';
+import { emailRegex, pwRegex } from '../utils/utils';
 
 const SignUp = ({ navigation: { navigate } }) => {
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const validateInputs = () => {
+    if (!email) {
+      console.log('이메일 입력해주세여');
+      return true;
+    }
+    if (!pw) {
+      console.log('패스워드 입력해주세여');
+
+      return true;
+    }
+    const matchedEmail = email.match(emailRegex);
+    const matchedPw = pw.match(pwRegex);
+
+    if (matchedEmail === null) {
+      console.log('이메일 형식 맞게 입력해주세여');
+
+      return true;
+    }
+    if (matchedPw === null) {
+      console.log('비밀번호는 여덟자 이상이어야되여 ');
+
+      pwRef.current.focus();
+      return true;
+    }
+  };
+  const handleRegister = () => {
+    // 유효성 검사
+    if (validateInputs()) {
+      return;
+    }
+
+    // 회원가입 요청
+    createUserWithEmailAndPassword(authService, email, pw)
+      .then(() => {
+        console.log('로그인성공');
+        setEmail('');
+        setPw('');
+      })
+      .catch((err) => {
+        console.log('err.message:', err.message);
+        if (err.message.includes('already-in-use')) {
+          console.log('이미 사용중인 아이디라능!!');
+        }
+      });
+  };
   return (
     <SignUpContainer>
       <ThereAreMainText />
       <InputContainer>
         <InputText>이메일</InputText>
-        <CustomInput />
+        <CustomInput
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          textContentType="emailAddress"
+          placeholder="Enter your email id"
+        />
         <InputText>비밀번호</InputText>
-        <CustomInput />
+        <CustomInput
+          value={pw}
+          onChangeText={(text) => setPw(text)}
+          textContentType="password"
+          returnKeyType="send"
+          secureTextEntry={true}
+          placeholder="Enter your password"
+        />
         <InputText>이름</InputText>
         <CustomInput />
         <LoginBottomContainer>
-          <JoinButtonContainer>
+          <JoinButtonContainer onPress={handleRegister}>
             <CustomButton>회원가입</CustomButton>
           </JoinButtonContainer>
           <TouchableOpacity
